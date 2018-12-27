1e00eba5440155b53e403f4b68c4c76c504db964
Joey Damico <damico31097@gmail.com>
2018-12-27 15:53:17 -0500

    Deck View
    Created the scroll view to show the deck of cards

--- app/resources/js/Casino.js
+++ app/resources/js/Casino.js
@@ -7,7 +7,19 @@ deck.initializeDeck();
 
 var i;
 for (i = 0; i < deck.deckSize(); i++) {
-	console.log(deck.get(i).getName());
+	var card_path = "images/cards/"
+	var card_prefix = "card_";
+	var card_name = "";
+
+	var card = deck.get(i).getAbbv();
+	card_name += card_path + card_prefix;
+	card_name += card.toLowerCase() + ".png";
+	console.log(card_name);
+
+
+	var card_image = document.createElement("img");
+	card_image.src = card_name;
+	document.getElementById("deckView").appendChild(card_image);
 }
 
 /*
